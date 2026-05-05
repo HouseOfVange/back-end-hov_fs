958c671cab8d964c0bd7b1aa58b7df4b440ed18c
vange <houseofvange@gmail.com>
2026-05-05 16:44:46 -0700

    HOV: cleans up price_sticker class

--- app/models/price_sticker.py
+++ app/models/price_sticker.py
@@ -1,8 +1,5 @@
 from app import db
 
-# formerly Board
 class Price_Sticker(db.Model):
     price_sticker_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     next_avail_price = db.Column(db.Integer)
-
-    comment = db.relationship("Comment", backref="piece", passive_deletes=True)
