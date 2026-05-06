5fbf7d9076707bbcab1cb106a9ec5dc6d0dee55d
vange <houseofvange@gmail.com>
2026-05-05 21:59:40 -0700

    HOV: rename PriceSticker class and add to_dict method

--- app/models/price_sticker.py
+++ app/models/price_sticker.py
@@ -1,5 +1,11 @@
 from app import db
 
-class Price_Sticker(db.Model):
+class PriceSticker(db.Model):
     price_sticker_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     next_avail_price = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "id": self.price_sticker_id,
+            "next_avail_price": self.next_avail_price
+        }
